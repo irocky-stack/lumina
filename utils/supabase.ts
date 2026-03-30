a4ebdf3f488090b97003d2e951686ebc18502880
irocky-stack <suleimanparker01@gmail.com>
2026-03-30 01:49:41 +0000

    supabase configured

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'your-supabase-url'
-const supabaseAnonKey = 'your-anon-key'
+const supabaseUrl = 'https://qvgsdhhmmfbaywkynlow.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2Z3NkaGhtbWZiYXl3a3lubG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MjY5MjQsImV4cCI6MjA5MDQwMjkyNH0.nDBxnSGq1q0hMm41srEJZpEgFMIyn5m9xWqQ-OoK4XE'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
